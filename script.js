856e5c15d7a91cad322922f80c37dce245036c54
David <xddavidxd2001@gmail.com>
2023-11-20 15:13:19 -0500

    Subida de archivos a s3

--- script.js
+++ script.js
@@ -1,166 +1,211 @@
-  function createSectionObject(
-    name,
-    ptlf,
-    title1,
-    input1,
-    input2,
-    title2,
-    input3,
-    input4
-  ) {
-    return {
-      name: name,
-      ptlf: ptlf,
-      title1: title1,
-      input1: input1,
-      input2: input2,
-      title2: title2,
-      input3: input3,
-      input4: input4,
+function createSectionObject(
+  name,
+  ptlf,
+  title1,
+  input1,
+  input2,
+  title2,
+  input3,
+  input4
+) {
+  return {
+    name: name,
+    ptlf: ptlf,
+    title1: title1,
+    input1: input1,
+    input2: input2,
+    title2: title2,
+    input3: input3,
+    input4: input4,
+  };
+}
+
+// Obtener los componentes de la fecha
+
+var today = new Date();
+var year = today.getFullYear();
+var month = (today.getMonth() + 1).toString().padStart(2, "0");
+var day = today.getDate().toString().padStart(2, "0");
+var formattedDate = `${year}-${month}-${day}`;
+
+function loadFile(url, callback) {
+  PizZipUtils.getBinaryContent(url, callback);
+}
+
+var downloadedFiles = [];
+
+function updateDownloadHistory(filename) {
+  downloadedFiles.push(filename);
+  showDownloadHistory();
+}
+
+function showDownloadHistory() {
+  var sidebar = document.querySelector(".sidebar");
+  var downloadHistoryContainer = sidebar.querySelector(
+    ".download-history-container"
+  );
+  var downloadHistory = document.createElement("div");
+  downloadHistory.className = "download-history";
+
+  if (downloadedFiles.length === 0) {
+    downloadHistory.innerHTML +=
+      '<p style="color: white;">Ningún archivo descargado aún.</p>';
+  } else {
+    for (var i = 0; i < downloadedFiles.length; i++) {
+      downloadHistory.innerHTML +=
+        '<p style="color: white;">' + downloadedFiles[i] + "</p>";
+    }
+  }
+
+  var existingDownloadHistory =
+    downloadHistoryContainer.querySelector(".download-history");
+  if (existingDownloadHistory) {
+    existingDownloadHistory.remove();
+  }
+
+  downloadHistoryContainer.appendChild(downloadHistory);
+}
+
+window.generate = function generate() {
+  if (sectionCount < 1) {
+    alert("Debes agregar al menos una sección antes de generar el documento.");
+    return;
+  }
+
+  const sections = [];
+
+  for (var i = 1; i <= sectionCount; i++) {
+    const nameValue = document.getElementById(`nameInput${i}`).value;
+    const ptlfValue = document.getElementById(`ptlf${i}`).value;
+    const title1Value = document.getElementById(`title1${i}`).value;
+    const Input1Value = document.getElementById(`Input1${i}`).value;
+    const Input2Value = document.getElementById(`Input2${i}`).value;
+    const title2Value = document.getElementById(`title2${i}`).value;
+    const Input3Value = document.getElementById(`Input3${i}`).value;
+    const Input4Value = document.getElementById(`Input4${i}`).value;
+    const section = createSectionObject(
+      nameValue,
+      ptlfValue,
+      title1Value,
+      Input1Value,
+      Input2Value,
+      title2Value,
+      Input3Value,
+      Input4Value
+    );
+    sections.push(section);
+
+    // Crear objeto con los valores
+    const sectionData = {
+      name: nameValue,
+      ptlf: ptlfValue,
+      title1: title1Value,
+      input1: Input1Value,
+      input2: Input2Value,
+      title2: title2Value,
+      input3: Input3Value,
+      input4: Input4Value,
     };
   }
 
-  // Obtener los componentes de la fecha
-
-  var today = new Date();
-  var year = today.getFullYear();
-  var month = (today.getMonth() + 1).toString().padStart(2, "0");
-  var day = today.getDate().toString().padStart(2, "0");
-  var formattedDate = `${year}-${month}-${day}`;
-
-  function loadFile(url, callback) {
-    PizZipUtils.getBinaryContent(url, callback);
-  }
-
-  var downloadedFiles = [];
-
-  function updateDownloadHistory(filename) {
-    downloadedFiles.push(filename);
-    showDownloadHistory();
-  }
-
-  function showDownloadHistory() {
-    var sidebar = document.querySelector(".sidebar");
-    var downloadHistoryContainer = sidebar.querySelector(".download-history-container");
-    var downloadHistory = document.createElement("div");
-    downloadHistory.className = "download-history";
-    
-  
-    if (downloadedFiles.length === 0) {
-      downloadHistory.innerHTML += '<p style="color: white;">Ningún archivo descargado aún.</p>';
-    } else {
-      for (var i = 0; i < downloadedFiles.length; i++) {
-        downloadHistory.innerHTML += '<p style="color: white;">' + downloadedFiles[i] + "</p>";
+  loadFile(
+    "https://docs.google.com/document/d/1yW7oodVfkG4epuOb8jrd1WONuej2zVyJ/export?format=docx",
+    function (error, content) {
+      if (error) {
+        throw error;
       }
-    }
-  
-    var existingDownloadHistory = downloadHistoryContainer.querySelector(".download-history");
-    if (existingDownloadHistory) {
-      existingDownloadHistory.remove();
-    }
-  
-    downloadHistoryContainer.appendChild(downloadHistory);
-  }
+      const zip = new PizZip(content);
+      const doc = new window.docxtemplater(zip, {
+        paragraphLoop: true,
+        linebreaks: true,
+      });
 
-  window.generate = function generate() {
-    if (sectionCount < 1) {
-      alert("Debes agregar al menos una sección antes de generar el documento.");
-      return;
-    }
+      var renderedSections = [];
 
-    const sections = [];
+      sections.forEach(function (section) {
+        var renderedSection = {
+          name: section.name,
+          PTLF: section.ptlf,
+          title1: section.title1,
+          input1: section.input1,
+          input2: section.input2,
+          title2: section.title2,
+          input3: section.input3,
+          input4: section.input4,
+        };
+        renderedSections.push(renderedSection);
+      });
 
-    for (var i = 1; i <= sectionCount; i++) {
-      const nameValue = document.getElementById(`nameInput${i}`).value;
-      const ptlfValue = document.getElementById(`ptlf${i}`).value;
-      const title1Value = document.getElementById(`title1${i}`).value;
-      const Input1Value = document.getElementById(`Input1${i}`).value;
-      const Input2Value = document.getElementById(`Input2${i}`).value;
-      const title2Value = document.getElementById(`title2${i}`).value;
-      const Input3Value = document.getElementById(`Input3${i}`).value;
-      const Input4Value = document.getElementById(`Input4${i}`).value;
-      const section = createSectionObject(
-        nameValue,
-        ptlfValue,
-        title1Value,
-        Input1Value,
-        Input2Value,
-        title2Value,
-        Input3Value,
-        Input4Value
-      );
-      sections.push(section);
-    }
+      doc.render({
+        Section: renderedSections,
+      });
 
-    loadFile(
-      "https://docs.google.com/document/d/1yW7oodVfkG4epuOb8jrd1WONuej2zVyJ/export?format=docx",
-      function (error, content) {
-        if (error) {
-          throw error;
+      const blob = doc.getZip().generate({
+        type: "blob",
+        mimeType:
+          "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        compression: "DEFLATE",
+      });
+
+      // Configurar AWS SDK
+      AWS.config.update({
+        accessKeyId: 'AKIA4S4JXO76SPL4VHTS',
+        secretAccessKey: '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',
+        region: 'us-east-1'
+      });
+
+      // Crear el objeto S3
+      var s3 = new AWS.S3();
+
+      var generatedFileName =
+        "Auditoria " + sections[0].name + " / " + formattedDate + ".docx";
+      updateDownloadHistory(generatedFileName);
+
+
+      // Crear el objeto de parámetros para la carga del archivo
+      var params = {
+        Bucket: 'auditflow',
+        Key: generatedFileName,
+        Body: blob,
+        ContentType: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        // No incluir ACL para utilizar la configuración predeterminada del bucket
+      };
+
+      saveAs(blob, generatedFileName);
+
+        // Subir el archivo a S3
+        s3.upload(params, function (err, data) {
+        if (err) {
+          console.error("Error al subir el archivo a S3: ", err);
+        } else {
+          console.log("Archivo subido exitosamente a S3: ", data);
+          updateDownloadHistory(generatedFileName);
         }
-        const zip = new PizZip(content);
-        const doc = new window.docxtemplater(zip, {
-          paragraphLoop: true,
-          linebreaks: true,
-        });
+      });
 
-        var renderedSections = [];
-
-        sections.forEach(function (section) {
-          var renderedSection = {
-            name: section.name,
-            PTLF: section.ptlf,
-            title1: section.title1,
-            input1: section.input1,
-            input2: section.input2,
-            title2: section.title2,
-            input3: section.input3,
-            input4: section.input4,
-          };
-          renderedSections.push(renderedSection);
-        });
-
-        doc.render({
-          Section: renderedSections,
-        });
-
-        const blob = doc.getZip().generate({
-          type: "blob",
-          mimeType:
-            "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-          compression: "DEFLATE",
-        });
-
-        var generatedFileName = "Auditoria " + sections[0].name + " / " + formattedDate + ".docx";
-        updateDownloadHistory(generatedFileName);
-
-        saveAs(
-          blob,
-          generatedFileName
-        );
-      }
-    );
-  };
-
-  function cleanInputs() {
-    for (var i = 1; i <= sectionCount; i++) {
-      document.getElementById(`ptlf${i}`).value = "";
-      document.getElementById(`nameInput${i}`).value = "";
-      document.getElementById(`Input1${i}`).value = "";
-      document.getElementById(`Input2${i}`).value = "";
-      document.getElementById(`Input3${i}`).value = "";
-      document.getElementById(`Input4${i}`).value = "";
     }
+  );
+};
+
+function cleanInputs() {
+  for (var i = 1; i <= sectionCount; i++) {
+    document.getElementById(`ptlf${i}`).value = "";
+    document.getElementById(`nameInput${i}`).value = "";
+    document.getElementById(`Input1${i}`).value = "";
+    document.getElementById(`Input2${i}`).value = "";
+    document.getElementById(`Input3${i}`).value = "";
+    document.getElementById(`Input4${i}`).value = "";
   }
+}
 
-  var sectionCount = 0;
+var sectionCount = 0;
 
-  function addSection() {
-    sectionCount++;
+function addSection() {
+  sectionCount++;
 
-    var newSection = document.createElement("h3");
-    newSection.className = "NuevaSec";
-    newSection.innerHTML = `
+  var newSection = document.createElement("h3");
+  newSection.className = "NuevaSec";
+  newSection.innerHTML = `
       <div class="card" id="sectionContainer">
           <h3  style="color:#2B68DB">Titulo de la transaccion ${sectionCount}</h3>
           <textarea id="nameInput${sectionCount}" spellcheck="false" placeholder="Nombre de la transaccion" class="input" name="firstName"  ></textarea> 
@@ -190,19 +235,21 @@
       </div>
     `;
 
-    document.body.appendChild(newSection);
-  }
+  document.body.appendChild(newSection);
+}
 
-  function eliminarElementosPorClase() {
-    const elementosEliminar = document.querySelectorAll("." + "NuevaSec");
+function eliminarElementosPorClase() {
+  const elementosEliminar = document.querySelectorAll("." + "NuevaSec");
 
-    elementosEliminar.forEach(function (elemento) {
-      elemento.remove();
-    });
+  elementosEliminar.forEach(function (elemento) {
+    elemento.remove();
+  });
 
-    sectionCount = 0;
-  }
+  sectionCount = 0;
+}
 
-  window.onload = function () {
-    showDownloadHistory();
-  };
+window.onload = function () {
+  showDownloadHistory();
+};
+
+
